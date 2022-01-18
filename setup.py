bb1d15c5afd812e99ef3a89a6316694be93ccf3f
bvittrant <30357897+bvittrant@users.noreply.github.com>
2022-01-18 15:56:49 +0100

    Create setup.py
    Added setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bvipackpy',
+      version='1.0',
+      description='Basic python functions',
+      author='Benjamin Vittrant',
+      author_email='bv.research@protonmail.com',
+      url='https://github.com/bvittrant/bvipackpy',
+      packages=['distutils', 'distutils.command','os'],
+     )
